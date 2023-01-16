8767fe61b4253759316a248613d52e3e4ee488a7
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-01-16 23:47:41 +0600

    start Kata 'ConsecutiveString'

--- src/Kata/6kyu/ConsecutiveStrings.js
+++ src/Kata/6kyu/ConsecutiveStrings.js
@@ -32,19 +32,3 @@ function longestConsec(strarr, k) {
 };
 
 console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2));
-
-//=======Are You Playing Banjo?================
-// Create a function which answers the question "Are you playing banjo?".
-// If your name starts with the letter "R" or lower case "r", you are playing banjo!
-
-// The function takes a name as its only argument, and returns one of the following strings:
-
-const areYouPlayingBanjo = (name) => {
-	return name[0].toLowerCase() === 'r' ? `${name} plays banjo` : `${name} does not play banjo`;
-};
-
-//============best practices============================================================
-// function areYouPlayingBanjo(name) {
-// 	return name + (/^r/i.test(name) ? " plays " : " does not play ") + "banjo";
-// }
-
