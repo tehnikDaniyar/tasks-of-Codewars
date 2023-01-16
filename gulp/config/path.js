60b65c5ccc84183b64ef92e5507d07aca2508674
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-01-16 10:57:44 +0600

    create config path, create task js

---
+++ gulp/config/path.js
@@ -0,0 +1,14 @@
+const srcFolder = "./src";
+const buildFolder = "./dist";
+
+export const path = {
+	build: {
+		js: `${buildFolder}/`,
+	},
+	src: {
+		js: `${srcFolder}/*.js`,
+	},
+	whatch: {
+		js: `${srcFolder}/**/*.js`,
+	}
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,9 @@
 //======main modules=======
 import gulp from 'gulp';
+import path from './gulp/config/path.js';
+
+//======glodal============
+global.app = {
+	gulp: gulp,
+	path: path,
+};
