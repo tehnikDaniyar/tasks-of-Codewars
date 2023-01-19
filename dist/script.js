5e18141f4c4832a68369e540a4db0c2cb3cffd43
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-01-19 09:06:15 +0600

    refactoring

--- dist/script.js
+++ dist/script.js
@@ -36,6 +36,37 @@ function longestConsec(strarr, k) {
 
 longestConsec(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 4)
 
+
+//=======Give me a Diamond============6 kyu=======
+/*Jamie is a programmer, and James' girlfriend. She likes diamonds, and wants a diamond string from James. Since James doesn't know how to make this happen, he needs your help.
+
+Task
+You need to return a string that looks like a diamond shape when printed on the screen, using asterisk (*) characters. Trailing spaces should be removed, and every line must be terminated with a newline character (\n).
+
+Return null/nil/None/... if the input is an even number or negative, as it is not possible to print a diamond of even or negative size.*/
+
+function diamond(n) {
+	let arr = [];
+	let a = '*';
+	let counter = -1;
+	let flag = true;
+	for (let i = 0; i < n; i++) {
+		if (flag) {
+			a += '**';
+			counter += 2;
+			counter === n ? flag = false : null;
+		} else {
+			a -= '**';
+			counter -= 2;
+		};
+		arr.push(a);
+	};
+	return arr;
+};
+
+diamond(5);
+
+
 //=======Are You Playing Banjo?================
 // Create a function which answers the question "Are you playing banjo?".
 // If your name starts with the letter "R" or lower case "r", you are playing banjo!
--- src/Kata/6kyu/GiveMeADiamond.js
+++ src/Kata/6kyu/GiveMeADiamond.js
@@ -1,6 +1,6 @@
 
 
-//=======Give me a Diamond================
+//=======Give me a Diamond============6 kyu=======
 /*Jamie is a programmer, and James' girlfriend. She likes diamonds, and wants a diamond string from James. Since James doesn't know how to make this happen, he needs your help.
 
 Task
@@ -9,5 +9,22 @@ You need to return a string that looks like a diamond shape when printed on the 
 Return null/nil/None/... if the input is an even number or negative, as it is not possible to print a diamond of even or negative size.*/
 
 function diamond(n) {
-	return '';
-}
+	let arr = [];
+	let a = '*';
+	let counter = -1;
+	let flag = true;
+	for (let i = 0; i < n; i++) {
+		if (flag) {
+			a += '**';
+			counter += 2;
+			counter === n ? flag = false : null;
+		} else {
+			a -= '**';
+			counter -= 2;
+		};
+		arr.push(a);
+	};
+	return arr;
+};
+
+diamond(5);
