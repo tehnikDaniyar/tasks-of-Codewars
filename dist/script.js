4f480e7ab9b6da5c2274773bdba38744b6f9bf26
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-02-04 12:59:39 +0600

    decided 'calculateBMI'

--- dist/script.js
+++ dist/script.js
@@ -531,6 +531,43 @@ const areYouPlayingBanjo = (name) => {
 // }
 
 
+
+
+
+
+//==========calculate BMI=========
+/*Write function bmi that calculates body mass index (bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"*/
+
+function bmi(weight, height) {
+	let BMI = weight / (height ** 2);
+	if (BMI <= 18.5) {
+		return "Underweight";
+	};
+	if (BMI > 18.5 && BMI <= 25) {
+		return "Normal";
+	};
+	if (BMI > 25 && BMI <= 30) {
+		return "Overweight";
+	};
+	if (BMI > 30) {
+		return "Obese";
+	}
+};
+
+
+console.log(bmi(80, 1.80));
+
+// const bmi = (w, h, bmi = w/h/h) =>  bmi <= 18.5 ? "Underweight" :
+//                                     bmi <= 25 ? "Normal" :
+//                                     bmi <= 30 ? "Overweight" : "Obese";
 /*Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.*/
 function boolToWord(bool) {
 	bool ? 'Yes' : 'No'
---
+++ src/Kata/8kyu/calculateBMI.js
@@ -0,0 +1,37 @@
+
+
+
+
+//==========calculate BMI=========
+/*Write function bmi that calculates body mass index (bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"*/
+
+function bmi(weight, height) {
+	let BMI = weight / (height ** 2);
+	if (BMI <= 18.5) {
+		return "Underweight";
+	};
+	if (BMI > 18.5 && BMI <= 25) {
+		return "Normal";
+	};
+	if (BMI > 25 && BMI <= 30) {
+		return "Overweight";
+	};
+	if (BMI > 30) {
+		return "Obese";
+	}
+};
+
+
+console.log(bmi(80, 1.80));
+
+// const bmi = (w, h, bmi = w/h/h) =>  bmi <= 18.5 ? "Underweight" :
+//                                     bmi <= 25 ? "Normal" :
+//                                     bmi <= 30 ? "Overweight" : "Obese";
