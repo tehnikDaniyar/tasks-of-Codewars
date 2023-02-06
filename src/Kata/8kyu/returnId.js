4d576cac35bb0516083a8a618837894795e08dd4
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-02-06 12:32:17 +0600

    decided kata 'returnTd'

---
+++ src/Kata/8kyu/returnId.js
@@ -0,0 +1,12 @@
+let counter = 0;
+function id() {
+	return counter++;
+};
+
+console.log(id());
+console.log(id());
+console.log(id());
+console.log(id());
+console.log(id());
+console.log(id());
+
