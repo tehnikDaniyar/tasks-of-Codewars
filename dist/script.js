8f264e9812ba60388a243bce3c7392f30d68bfe4
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-02-10 21:16:17 +0600

    decided 'twoFightersOneWinner'

--- dist/script.js
+++ dist/script.js
@@ -646,6 +646,47 @@ let a = trim("OU aMhEv", 3);
 
 
 
+//============twoFightersOneWnner===============
+/*Create a function that returns the name of the winner in a fight between two fighters.
+
+Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
+
+Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+
+Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
+
+Your function also receives a third argument, a string, with the name of the fighter that attacks first.*/
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+	let first = fighter1.name === firstAttacker ? fighter1 : fighter2;
+	let second = first.name === fighter1.name ? fighter2 : fighter1;
+
+	while (first.health > 0 && second.health > 0) {
+		second.health -= first.damagePerAttack;
+		if (second.health <= 0) {
+			break;
+		} else {
+			first.health -= second.damagePerAttack;
+		}
+	};
+
+
+	return first.health <= 0 ? second.toString() : first.toString();
+};
+
+let winner = declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Lew");
+
+
+function Fighter(name, health, damagePerAttack) {
+	this.name = name;
+	this.health = health;
+	this.damagePerAttack = damagePerAttack;
+	this.toString = function () { return this.name; }
+};
+
+console.log(winner);
+
+
 //=======Are You Playing Banjo?================
 // Create a function which answers the question "Are you playing banjo?".
 // If your name starts with the letter "R" or lower case "r", you are playing banjo!
