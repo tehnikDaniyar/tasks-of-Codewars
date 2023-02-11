2f964867e17c12e12318d3c7ec1e06b5cbddfbf5
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-02-11 23:29:23 +0600

    decided kata 'VolumeOfACuboid'

--- dist/script.js
+++ dist/script.js
@@ -894,3 +894,14 @@ const problem = (x) => {
 };
 
 let sdsdsds = problem(10);
+
+
+
+//================Volume of a Cuboid===============
+/*Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.*/
+
+class Kata {
+	static getVolumeOfCuboid(length, width, height) {
+		return length * width * height
+	}
+}
