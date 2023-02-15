360f271f6c94bb84936337a45d6ad1bfe8fcdc51
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-02-15 20:58:58 +0600

    refactoring 'MaximumLengthDiffre'

--- dist/script.js
+++ dist/script.js
@@ -511,19 +511,12 @@ Find max(abs(length(x) − length(y)))
 If a1 and/or a2 are empty return -1 in each language except in Haskell (F#) where you will return Nothing (None).*/
 
 function mxdiflg(a1, a2) {
-	let minA1 = (a1.reduce((res, elem) => res.length < elem.length ? res = res : res = elem)).length;
-	let maxA1 = (a1.reduce((res, elem) => res.length > elem.length ? res = res : res = elem)).length;
-
-	let minA2 = (a2.reduce((res, elem) => res.length < elem.length ? res = res : res = elem)).length;
-	let maxA2 = (a2.reduce((res, elem) => res.length > elem.length ? res = res : res = elem)).length;
-
-	let x = minA1 < minA2 ? minA1 : minA2;
-	let y = maxA1 > maxA2 ? maxA1 : maxA2;
-
-	return y - x;
+	a1 = a1.map(elem => elem.length);
+	a2 = a2.map(elem => elem.length);
+	return Math.max(...a1, ...a2) - Math.min(...a1, ...a2);
 };
 
-mxdiflg(['123', '1234', '12345'], ['123', '123456']);
+mxdiflg(['123', '1234', '123456789', '12'], ['123', '123456', '1']);
 
 
 
