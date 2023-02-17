35b8f6e991d4c993d0b1ad228fac84b4c32a78ca
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-02-17 23:46:23 +0600

    created kata 'Construct Submatrix'

---
+++ src/Kata/7kyu/ConstructSubmatrix.js
@@ -0,0 +1,39 @@
+
+
+
+//============Construct Submatrix====================
+/*Task
+Given a matrix, find its submatrix obtained by deleting the specified rows and emptying the specified columns.
+
+Input/Output
+[input] 2D integer array matrix
+
+2-dimensional array of integers.
+
+1 ≤ matrix.length ≤ 10,
+
+1 ≤ matrix[0].length ≤ 10,
+
+0 ≤ matrix[i][j] ≤ 9.
+
+[input] integer array rowsToDelete
+
+Array of indices (0-based) of rows to be deleted.
+
+0 ≤ rowsToDelete.length ≤ 5,
+
+0 ≤ rowsToDelete[i] < matrix.length.
+
+[input] integer array columnsToDelete
+
+Array of indices (0-based) of columns to be deleted.
+
+0 ≤ columnsToDelete.length ≤ 5,
+
+0 ≤ columnsToDelete[i] < matrix[0].length.
+
+[output] 2D integer array*/
+
+function constructSubmatrix(matrix, rowsToDelete, columnsToDelete) {
+
+}
