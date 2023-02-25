6c9fd3b8d40c9afe6446be5a69a61e460cbad887
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-02-25 22:36:26 +0600

    created kata '2d_1d_arrayCoordinatesMapping'

--- dist/script.js
+++ dist/script.js
@@ -478,6 +478,21 @@ function expandedForm(num) {
 let res = expandedForm(20324);
 
 
+//===========2D / 1D array coordinates mapping====================
+/*Hello! Your are given x and y and 2D array size tuple (width, height) and you have to:
+
+Calculate the according index in 1D space (zero-based).
+Do reverse operation.*/
+
+function to1D(x, y, size) {
+	return;
+}
+
+function to2D(n, size) {
+	return;
+}
+
+
 
 //========Cats and shelves===============
 /*An infinite number of shelves are arranged one above the other in a staggered fashion.
---
+++ src/Kata/7kyu/2D_1D_arrayCoordinatesMapping.js
@@ -0,0 +1,15 @@
+
+
+//===========2D / 1D array coordinates mapping====================
+/*Hello! Your are given x and y and 2D array size tuple (width, height) and you have to:
+
+Calculate the according index in 1D space (zero-based).
+Do reverse operation.*/
+
+function to1D(x, y, size) {
+	return;
+}
+
+function to2D(n, size) {
+	return;
+}
