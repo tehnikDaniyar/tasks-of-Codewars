5da33130c543e311f0e9274ec6ade676e1d3b471
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-03-12 13:28:39 +0600

    refactoring 'Convert string to camel case'

--- src/Kata/6kyu/Convert string to camel case.js
+++ src/Kata/6kyu/Convert string to camel case.js
@@ -5,16 +5,15 @@
 /*Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case). The next words should be always capitalized.*/
 
 function toCamelCase(str) {
-	console.log("Convert string to camel case");
-	if (str.length === 0) {
-		return "";
-	};
-	let onlyWordString = str.replace(/[^a-zA-Z]/g, ' ');
-	let arr = onlyWordString.split(" ").map((word, index) => index !== 0 ? word[0].toUpperCase() + word.slice(1) : word);
-	let result = arr.join("");
-	return result;
+	str.length === 0 ? "" :
+		str.replace(/[^a-zA-Z]/g, ' ')
+			.map((word, index) => index !== 0 ? word[0].toUpperCase() + word.slice(1) : word)
+			.split(" ")
+			.join("");
 };
 
-toCamelCase("the-stealth-warrior");
+// function toCamelCase(str) {
+// 	return str.replace(/[-_](.)/g, (_, c) => c.toUpperCase());
+// }
 
 
